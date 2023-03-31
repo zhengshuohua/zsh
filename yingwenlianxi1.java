8ca298e04bee130ad393235b149068d664cb7d69
ZHENGSHUOHUA\1 <zhengshuohua1998@gmail.com>
2023-04-01 05:54:28 +0900

    2

---
+++ yingwenlianxi1.java
@@ -0,0 +1,10 @@
+public class yingwenlianxi1{
+	public static void main(String[]args) {
+		System.out.println("Hello World");
+		System.out.println("Let's paly hide and seek.");
+		System.out.print("Three...");
+		System.out.print("Two...");
+		System.out.print("One...");
+		System.out.println("Ready pr not,here i come!");
+	}
+}
---
+++ yingwenlianxi2.java
@@ -0,0 +1,18 @@
+public class yingwenlianxi2{
+	public static void main(String[]args) {
+		System.out.println("这是我学习代码的第3天");
+		System.out.println("今日も楽しい一日だ");
+		//语法
+		System.out.print("111");
+		/*语法语法语法语法语法语法语法
+		 * 4444
+		 */
+		System.out.println("Semicolons and Whitespace");
+		
+		System.out.println("Semicolons and Whitespace");
+		
+		System.out.println("Semicolons and Whitespace");
+	}
+	
+	
+}
